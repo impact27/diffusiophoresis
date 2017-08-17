6c5675f65e5aeb9cd57430a2cb41246da430fceb
Quentin Peter <qaep2@cam.ac.uk>
2017-08-17 14:40:41 +0100

    Marchouille

--- Numerical/diffusiosim.py
+++ Numerical/diffusiosim.py
@@ -7,22 +7,22 @@ Created on Tue Aug 15 14:18:26 2017
 import numpy as np
 from scipy.linalg import toeplitz
 from numpy.linalg import eigvals
+import matplotlib.pyplot as plt
 
 class diffusioSim():
     def __init__(self, X, CsaltIN, CsaltOUT, Dprot, 
                  Dsalt, Ddiffph, reservoir=False):
         super().__init__()
-        #Init salt concentration
-        self._Csalt=np.ones_like(X)*CsaltIN
-        self._Csalt[0]=CsaltOUT
         
         #init proteins concentration                         
         self._Cprot=np.zeros_like(X)
         self._Cprot[0]=1 
         
         self._X = X
+        self._t = 0
         
-        
+        self._Cin = CsaltIN
+        self._Cout = CsaltOUT
         self._Ddiffph = Ddiffph
         self._Dprot = Dprot
         self._Dsalt = Dsalt
@@ -39,6 +39,7 @@ class diffusioSim():
         fsalt = -Dsalt*sigmap
         self._dFsalt = self._getdF(X, fsalt)
 
+        self._L = X[-1] - X[0]
         
         
         
@@ -46,7 +47,7 @@ class diffusioSim():
         self._dtsalt = np.min(dx)**2/Dsalt/2
         
         
-    
+#    @profile
     def advance(self, t):
         
         q = self._q
@@ -56,8 +57,7 @@ class diffusioSim():
         Dprot = self._Dprot
         Ddiffph = self._Ddiffph
         while t>0:
-            #Get dxlnqs
-            dxlnQs = sigmap@self._Csalt/self._Csalt
+
             
             #Get dt
             dt0 = np.min(dx)**2/Dprot/2
@@ -66,45 +66,54 @@ class diffusioSim():
 #            print(dt0, dt1)
             dt = dt0
             
-            #Update salt
-            Nsaltdt = int(np.ceil(dt/self._dtsalt))
-            dtsalt = dt/Nsaltdt
-            Fsalt = np.linalg.matrix_power(q[0] + dtsalt*self._dFsalt, Nsaltdt)
-            self._Csalt = Fsalt@self._Csalt
+            GlnC, GGlnC = getdiffs(self._X,self._t, self._Dsalt, self._L, 
+                                   self._Cin, self._Cout)
             
-            #Get dxlnqs
-            dxlnQs = sigmap@self._Csalt/self._Csalt
-            
-            #Update proteins
-            fprot = (- Dprot*sigmap + Ddiffph*dxlnQs[:, np.newaxis]*q[int(neg)])
-            dF = self._getdF(self._X, fprot)
             
             
             
             
             Cxx = getCxx(len(self._X), np.max(dx))
             Cx = getCx(len(self._X), np.max(dx))
-            GlnC = Cx@self._Csalt/self._Csalt
-            GGlnC = (Cxx@self._Csalt/self._Csalt 
-                             -(Cx@self._Csalt)**2/self._Csalt**2)
+            Cx = 1/np.max(dx)/12*(-q[2]+8*q[1]-8*q[-1]+q[-2])
+#            Cx = 1/np.max(dx)/4*(q[1]+3*q[0]-5*q[-1]+q[-2])
+#            Cx = 1/np.max(dx)/2*(q[1]-q[-1])
+#            Cx = 1/np.max(dx)*(q[0]-q[-1])
+#            Cx = 1/np.max(dx)*(q[1]-q[0])
             
+            Cx[0]=0
+#            Cx2[0]=0
+            GlnC, GGlnC = getdiffs(self._X,self._t, self._Dsalt, self._L, 
+                                   self._Cin, self._Cout)
 
             dF2 = (self._Dprot*Cxx
                      + self._Ddiffph*(
                              GlnC[:, np.newaxis]*Cx 
                              + GGlnC[:, np.newaxis]*q[0]))
             
+            #Update proteins
+            dF = self._Dprot*Cxx + Dx(self._X, dt, t, self._Dsalt, self._L, 
+                    self._Cin, self._Cout, self._Ddiffph)
+            
+            dF2[0]=0
+            dF[0]=0
+            
+            
             
             F = q[0] + dt*dF2
+            
+            assert(len(np.shape(F))==2)
             self._Cprot = F@self._Cprot
             
             t = t-dt
-            
-        print(np.max(np.abs(eigvals(F))),np.max(self._Csalt))
-    
-    @property 
-    def Csalt(self):
-        return self._Csalt
+            self._t += dt
+#        plt.figure(100)
+#        plt.imshow(Cx-Cx2)
+#        plt.figure(101)
+#        plt.imshow(Cx)
+#        plt.figure(102)
+#        plt.imshow(Cx2)   
+        print(np.max(np.abs(eigvals(F))),np.max(self._Cprot))
     
     @property 
     def Cprot(self):
@@ -117,6 +126,84 @@ class diffusioSim():
         dx = np.append(dx, dx[-1])
         dF = 1/dx[:, np.newaxis]*(q[-1]@fp - fp)
         return dF
+    
+def Dx(X, dt, t, D, L, Cin, Cout, Ddp):
+    nx = len(X)
+    dx = X[1]-X[0]
+    q = getQs(nx)
+    
+    Xp = X[:-1] + dx/2
+    dxlnqhalf, _ = getdiffs(Xp, t+dt/2, D, L, Cin, Cout, nN=100)
+    #Correct
+    up = -Ddp * np.append(dxlnqhalf, 0)
+    #Incorrect BUT don't care
+    um = -Ddp * np.insert(dxlnqhalf, 0, 0)
+    
+#    dxlnq, _ = getdiffs(X, t, D, L, Cin, Cout, nN=100)
+#    up = -Ddp * dxlnq
+#    um = -Ddp * dxlnq
+    up = up[:, np.newaxis]
+    um = um[:, np.newaxis]
+    
+    
+
+    sigdx = np.zeros((3, nx, nx))
+    for i in range(-1, 2):
+        # Fromm choise of sigma
+        sigdx[i] = (q[i + 1] - q[i - 1]) / 2
+
+
+    neg = -Ddp*(Cin-Cout) < 0
+    assert not neg
+
+    Dx = (um*q[-1 + neg] - up*q[0 + neg]
+          + (.5 - neg) * ((um*sigdx[-1 + neg] - up*sigdx[0 + neg])
+                          - dt / dx * (um**2*sigdx[-1 + neg] 
+                          - up**2*sigdx[0 + neg])))
+    
+#    Dx = (um*q[0] - up*q[1])
+    Dx /= dx
+    return Dx 
+
+def Ct(t, N, D, L):
+    return np.exp(-D*(np.pi*(1/2+N)/L)**2*t)
+
+def C(x,t, D, L, nN=100):
+    x = x[:, np.newaxis]
+    N = np.arange(nN)[np.newaxis, :]
+    ret=(1/(N+0.5))*Ct(t, N, D, L)*np.sin((np.pi/2+N*np.pi)/L*x)
+    return (2/np.pi)*np.sum(ret, 1)
+#@profile 
+def dC(x,t, D, L, nN=100):
+    x = x[:, np.newaxis]
+    N = np.arange(nN)[np.newaxis, :]
+    ret = (Ct(t, N, D, L) * np.cos((np.pi/2+N*np.pi)/L*x))
+    return (2/L)*np.sum(ret, 1)
+
+def ddC(x,t, D, L, nN=100):
+    x = x[:, np.newaxis]
+    N = np.arange(nN)[np.newaxis, :]
+    ret=(N+0.5)*Ct(t, N, D, L)*np.sin((np.pi/2+N*np.pi)/L*x)
+    return (-2*np.pi/L**2)*np.sum(ret, 1)
+
+#@profile
+def getdiffs(x,t, D, L, Cin, Cout, nN=100):
+    x = x[:, np.newaxis]
+    N = np.arange(nN)[np.newaxis, :]
+    
+    Cts = Ct(t, N, D, L)
+    xN = (np.pi/2+N*np.pi)/L*x
+    
+    cos = np.cos(xN)
+    sin = np.sin(xN)
+    
+    Cs = (2/np.pi)*np.sum((1/(N+0.5))*Cts*sin, 1)
+    dCs = (2/L)*np.sum(Cts * cos, 1)
+    ddCs = (-2*np.pi/L**2)*np.sum((N+0.5)*Cts*sin, 1)
+
+    dlnCs = dCs/(Cout/(Cin-Cout) + Cs)
+    
+    return dlnCs, (ddCs/(Cout/(Cin-Cout) + Cs)-(dlnCs)**2)
     
 #get differential operators matrix
 def getCxx(nx, dx, reservoir=False):
@@ -149,24 +236,7 @@ def getCx(nx, dx, reservoir=False):
         Cx[-1,:]=0
     return Cx
 
-def getQs(nX, boundary='Neumann'):
-    """Get matrices to access neibours in y with correct boundary conditions
-    
-    Parameters
-    ----------
-    Zgrid:  integer
-        Number of Z pixel
-    Ygrid:  integer
-        Number of Y pixel
-    bounday: 'Neumann' or 'Dirichlet'
-        constant derivative or value
-        
-        
-    Returns
-    -------
-    qy:  3d array
-        A list of matrices to access [-2, +2] y neighbors
-    """
+def getQs(nX):
     #Create the q matrices
     q = np.zeros((5, nX, nX))
     for i in range(-2, 3):
--- Numerical/numerical diffusion.py
+++ Numerical/numerical diffusion.py
@@ -38,12 +38,12 @@ reservoir=False  # Dead end or reservoir
 
 
 
-X = np.exp(np.linspace(np.log(1e-6), np.log(501e-6), nx))-1e-6
+#X = np.exp(np.linspace(np.log(1e-6), np.log(501e-6), nx))-1e-6
 X = np.linspace(0, L, nx)
 
 dx = L/nx
 Cx = ds.getCx(nx, dx, reservoir)
-x=(np.arange(nx)*dx)[:,None]
+
 #get t for plotting
 t=np.exp(np.linspace(0,np.log(tmax),nt))
 
@@ -51,23 +51,28 @@ simulation = ds.diffusioSim(X, CsaltIN, CsaltOUT, Dprot,
                             Dsalt, Ddiffph, reservoir)
              
   
+
 #Start loop                          
-for i, dt in enumerate(np.diff(t)):
+for i, dt in enumerate([t[0], *np.diff(t)]):
     simulation.advance(dt)
     c=cmap(i/(nt-1))
+    Csalt = CsaltOUT + (CsaltIN-CsaltOUT)*ds.C(X, t[i], Dsalt, L)
+    dCsalt = (CsaltIN-CsaltOUT)*ds.dC(X, t[i], Dsalt, L)
+    dlnCsalt = dCsalt/Csalt
     plt.figure(1)
-    plt.plot(x*1e6, simulation.Csalt, c=c)
+    plt.plot(X*1e6, Csalt, c=c)
     plt.figure(2)
-    plt.plot(x[1:]*1e6, (Cx@simulation.Csalt)[1:], c=c)
+    plt.plot(X*1e6, dCsalt, c=c)
     plt.figure(3)
-    plt.plot(x[1:]*1e6, (Cx@simulation.Csalt/simulation.Csalt)[1:], c=c)
+    plt.plot(X*1e6, dlnCsalt, c=c)
     plt.figure(0)
-    plt.plot(x*1e6, simulation.Cprot, c=c)
+    plt.plot(X*1e6, simulation.Cprot, c=c)
     
         
 plt.figure(0)
 plt.xlabel(r'Position [$\mu$m]') 
 plt.xlim([0,500]) 
+#plt.ylim([0,3.5])
 plt.savefig('prot{:d}.pdf'.format(nx))
 plt.figure(1)
 plt.xlim([0,500])
@@ -77,6 +82,7 @@ plt.xlim([0,500])
 plt.savefig('dsalt{:d}.pdf'.format(nx))
 plt.figure(3)
 plt.xlim([0,500])
+plt.ylim([0,300000])
 plt.savefig('dlnsalt{:d}.pdf'.format(nx))
 
 
